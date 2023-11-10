02e79ace5488da6d6cb5b7e49eb6e3e25d03e90f
Alper Keskin <alper.keskin@siemens.com>
2023-11-10 21:56:36 +0300

    fix: add endpoint documentation

--- src/main/java/io/github/alprKeskin/kasimpamuk/thesettlersofcatan/rest/CatanController.java
+++ src/main/java/io/github/alprKeskin/kasimpamuk/thesettlersofcatan/rest/CatanController.java
@@ -30,40 +30,68 @@ public class CatanController {
         return ResponseEntity.ok("The server is up!");
     }
 
+    /***
+     * Adds the user to the Users table in the database.
+     * @param registrationInformation: email and password.
+     * @return: Success of the registration.
+     */
     @PostMapping("/register")
     public boolean register(@RequestBody RegistrationInformation registrationInformation) {
         return registerService.registerUser(registrationInformation);
     }
 
+    /***
+     * List users ordered by their weekly scores.
+     * @return: List of users ordered by weekly scores.
+     */
     @GetMapping("/weekly-leaderboard")
     public List<User> getWeeklyLeaderBoard() {
         return leaderboardService.getLeaderboardOfTheWeek();
     }
 
+    /***
+     * List users ordered by their monthly scores.
+     * @return: List of users ordered by monthly scores.
+     */
     @GetMapping("/monthly-leaderboard")
     public List<User> getMonthlyLeaderBoard() {
         return leaderboardService.getLeaderboardOfTheMonth();
     }
 
+    /***
+     * List users ordered by their all times scores.
+     * @return: List of users ordered by all times scores.
+     */
     @GetMapping("/all-time-leaderboard")
     public List<User> getAllTimeLeaderBoard() {
         return leaderboardService.getLeaderboardOfAllTimes();
     }
 
+    /***
+     * Adds a user
+     * @param user: User to be added.
+     * @return: Success of the addition.
+     */
     @PostMapping("add-user")
     public boolean addUser(@RequestBody User user) {
         return userManagerService.addUser(user);
     }
 
+    /***
+     * Adds a list of users.
+     * @param userList: List of users to be added.
+     * @return: List of users that cannot be added to the database.
+     */
     @PostMapping("add-users")
-    public ResponseEntity<String> addUsers(@RequestBody List<User> userList) {
-        List<User> unsuccessfulUserAdditions = userManagerService.addUsers(userList);
-        if (unsuccessfulUserAdditions.isEmpty()) {
-            return ResponseEntity.ok("Users are added successfully.");
-        }
-        return ResponseEntity.ok("Following users could not be added: " + unsuccessfulUserAdditions.toString());
+    public List<User> addUsers(@RequestBody List<User> userList) {
+        return userManagerService.addUsers(userList);
     }
 
+    /***
+     * Deletes a user.
+     * @param email: Email of the user to be deleted.
+     * @return: Success of the deletion.
+     */
     @PostMapping("delete-user")
     public boolean deleteUser(@RequestBody String email) {
         return userManagerService.deleteUser(email);
